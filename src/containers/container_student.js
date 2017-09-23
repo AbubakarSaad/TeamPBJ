85e40cd7b8f11623046116b86c906936db982f36
Abu <abubakar.saad.891@gmail.com>
2017-09-23 17:27:22 -0400

    Jquery integrated

--- src/containers/container_student.js
+++ src/containers/container_student.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
+import Navbar from '../components/navbar';
+
 
 class StudentPage extends Component{
 
   render(){
     return(
-      <div>
-      Student Page
+      <div className="container-fluid">
+        <div className="row">
+          <Navbar />
+          <div className="col-9 col-sm-10 col-md-10 col-lg-11 col-xl-11">
+              <h2>Hello There</h2>
+              <p>Test test test test test test test </p>
+          </div>
+        </div>
       </div>
     );
   }
