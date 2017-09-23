88c6fb5b7072792b98b63cd09e626bb08d219576
dan12t3 <dan12t3@gmail.com>
2017-09-23 15:00:20 -0400

    integerated middleware for redux

--- src/index.js
+++ src/index.js
@@ -4,5 +4,32 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { Router , Route, Switch } from 'react-router-dom';
+import reducers from './reducers';
+import ReduxPromise from 'redux-promise';
+import { syncHistoryWithStore, routerMiddleware } from 'react-router-redux';
+import createBrowserHistory from 'history/createBrowserHistory';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+const storeWithMiddleWare = applyMiddleware(ReduxPromise,routerMiddleware(createBrowserHistory({forceRefresh:true})))(createStore);
+
+const store = storeWithMiddleWare(reducers);
+
+const history = syncHistoryWithStore(createBrowserHistory(), store);
+
+
+ReactDOM.render(
+  <Provider>
+  <Router history={history}>
+
+  <App />
+
+  </Router>
+  </Provider>
+
+  , document.getElementById('root'));
 registerServiceWorker();
