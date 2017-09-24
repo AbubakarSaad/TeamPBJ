3274ce0c1d3f16091acf3d229fa4865009039a58
jeffyang62 <jeffyang62@gmail.com>
2017-09-23 22:16:20 -0400

    Update Education.js

--- src/components/Education.js
+++ src/components/Education.js
@@ -6,19 +6,23 @@ class Education extends Component {
 
     render() {
         return (
-            <div className="row">
-                <h2> Education </h2>
-                <div className="row"> 
-                    <div className="col-md-3 col">
-                        <img className="img-responsive" style={{width:"50%"}} src={mcmasterlogo} alt="mcmaster"/>
-                    </div>
-                    <div className="col-md-9" style={{marginLeft:"-14%"}}>    
-                        <p>McMaster University<br />
-                        Computer Science <br />
-                        Class of 2019 </p>
-                    </div>
-                </div>    
-            </div>            
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-12">
+                        <h2> Education </h2>
+                    </div> 
+                    <div className="row"> 
+                        <div className="col-md-3 col">
+                            <img className="img-responsive" style={{width:"50%"}} src={mcmasterlogo} alt="mcmaster"/>
+                        </div>
+                        <div className="col-md-9" style={{marginLeft:"-14%"}}>    
+                            <p>McMaster University<br />
+                            Computer Science <br />
+                            Class of 2019 </p>
+                        </div>
+                    </div>    
+                </div>   
+            </div>             
         );
     }
 };
